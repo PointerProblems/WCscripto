0383a7001dfa1002f86fec4f41f5d2fe1c118db3
PointerProblems <128872859+PointerProblems@users.noreply.github.com>
2024-06-14 23:05:04 -0400

    Shitty Weaponcore script

---
+++ SetGridsOn.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections.Generic;
+using System;
+
+WcPbApi wcApi = new WcPbApi();
+
+public Program()
+{
+    // Initialize the script
+    Runtime.UpdateFrequency = UpdateFrequency.None;
+
+    if (!wcApi.Activate(Me))
+    {
+        Echo("WeaponCore API failed to activate.");
+    }
+    else
+    {
+        Echo("WeaponCore API is ready.");
+    }
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+    // Find all WeaponCore weapons
+    var weapons = new List<IMyTerminalBlock>();
+    GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(weapons, block => wcApi.HasCoreWeapon(block));
+
+    if (weapons.Count == 0)
+    {
+        Echo("No WeaponCore weapons found.");
+        return;
+    }
+
+    foreach (var weapon in weapons)
+    {
+        var properties = new List<ITerminalProperty>();
+        weapon.GetProperties(properties);
+
+        var wcGridsProperty = properties.FirstOrDefault(p => p.Id == "WC_Grids");
+
+        if (wcGridsProperty != null)
+        {
+            bool wcGridsValue = weapon.GetValue<bool>(wcGridsProperty.Id);
+            if (!wcGridsValue)
+            {
+                var action = weapon.GetActionWithName("Grids");
+                action?.Apply(weapon);
+                Echo($"Set 'WC_Grids' to true for weapon: {weapon.CustomName}");
+            }
+        }
+        else
+        {
+            Echo($"Property 'WC_Grids' not found for weapon: {weapon.CustomName}");
+        }
+    }
+}
+
+public class WcPbApi
+{
+    public string[] WcBlockTypeLabels = new string[]
+    {
+                "Any",
+                "Offense",
+                "Utility",
+                "Power",
+                "Production",
+                "Thrust",
+                "Jumping",
+                "Steering"
+    };
+
+    private Action<ICollection<MyDefinitionId>> _getCoreWeapons;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, IDictionary<string, int>, bool> _getBlockWeaponMap;
+    private Action<IMyTerminalBlock, IDictionary<MyDetectedEntityInfo, float>> _getSortedThreats;
+    private Func<long, bool> _hasGridAi;
+    private Func<long, int, MyDetectedEntityInfo> _getAiFocus;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, long, int, bool> _setAiFocus;
+    private Action<Sandbox.ModAPI.Ingame.IMyTerminalBlock, bool, bool, int> _toggleWeaponFire;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, bool> _hasCoreWeapon;
+    private Action<Sandbox.ModAPI.Ingame.IMyTerminalBlock, ICollection<MyDetectedEntityInfo>> _getObstructions;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, ICollection<string>, int, bool> _getTurretTargetTypes;
+    private Action<Sandbox.ModAPI.Ingame.IMyTerminalBlock, ICollection<string>, int> _setTurretTargetTypes;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, long, int, Vector3D?> _getPredictedTargetPos;
+
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, int, Matrix> _getWeaponAzimuthMatrix;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, int, Matrix> _getWeaponElevationMatrix;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, long, int, MyTuple<bool, Vector3D?>> _isTargetAlignedExtended;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, int, string> _getActiveAmmo;
+    private Action<Sandbox.ModAPI.Ingame.IMyTerminalBlock, int, string> _setActiveAmmo;
+    private Func<long, float> _getConstructEffectiveDps;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, int, MyDetectedEntityInfo> _getWeaponTarget;
+    private Action<Sandbox.ModAPI.Ingame.IMyTerminalBlock, long, int> _setWeaponTarget;
+
+    private Action<Sandbox.ModAPI.Ingame.IMyTerminalBlock, int, Action<long, int, ulong, long, Vector3D, bool>> _monitorProjectile;
+    private Action<Sandbox.ModAPI.Ingame.IMyTerminalBlock, int, Action<long, int, ulong, long, Vector3D, bool>> _unMonitorProjectile;
+    private Func<ulong, MyTuple<Vector3D, Vector3D, float, float, long, string>> _getProjectileState;
+    private Func<long, MyTuple<bool, int, int>> _getProjectilesLockedOn;
+
+    private Action<Sandbox.ModAPI.Ingame.IMyTerminalBlock, bool, int> _fireWeaponOnce;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, int, bool, bool, bool> _isWeaponReadyToFire;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, int, float> _getMaxWeaponRange;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, int, MyTuple<Vector3D, Vector3D>> _getWeaponScope;
+    private Func<Sandbox.ModAPI.Ingame.IMyTerminalBlock, float> _currentPowerConsumption;
+
+    public bool IsReady { get; private set; }
+
+    public bool Activate(IMyTerminalBlock pbBlock)
+    {
+        var dict = pbBlock.GetProperty("WcPbAPI")?.As<IReadOnlyDictionary<string, Delegate>>().GetValue(pbBlock);
+        if (dict == null) throw new Exception("WcPbAPI failed to activate");
+        return ApiAssign(dict);
+    }
+
+    private bool ApiAssign(IReadOnlyDictionary<string, Delegate> delegates)
+    {
+        if (delegates == null)
+            return false;
+        AssignMethod(delegates, "GetCoreWeapons", ref _getCoreWeapons);
+        AssignMethod(delegates, "GetBlockWeaponMap", ref _getBlockWeaponMap);
+        AssignMethod(delegates, "GetSortedThreats", ref _getSortedThreats);
+        AssignMethod(delegates, "GetObstructions", ref _getObstructions);
+        AssignMethod(delegates, "HasGridAi", ref _hasGridAi);
+        AssignMethod(delegates, "GetAiFocus", ref _getAiFocus);
+        AssignMethod(delegates, "SetAiFocus", ref _setAiFocus);
+        AssignMethod(delegates, "HasCoreWeapon", ref _hasCoreWeapon);
+        AssignMethod(delegates, "GetPredictedTargetPosition", ref _getPredictedTargetPos);
+        AssignMethod(delegates, "GetTurretTargetTypes", ref _getTurretTargetTypes);
+        AssignMethod(delegates, "SetTurretTargetTypes", ref _setTurretTargetTypes);
+        AssignMethod(delegates, "GetWeaponAzimuthMatrix", ref _getWeaponAzimuthMatrix);
+        AssignMethod(delegates, "GetWeaponElevationMatrix", ref _getWeaponElevationMatrix);
+        AssignMethod(delegates, "IsTargetAlignedExtended", ref _isTargetAlignedExtended);
+        AssignMethod(delegates, "GetActiveAmmo", ref _getActiveAmmo);
+        AssignMethod(delegates, "SetActiveAmmo", ref _setActiveAmmo);
+        AssignMethod(delegates, "GetConstructEffectiveDps", ref _getConstructEffectiveDps);
+        AssignMethod(delegates, "GetWeaponTarget", ref _getWeaponTarget);
+        AssignMethod(delegates, "SetWeaponTarget", ref _setWeaponTarget);
+        AssignMethod(delegates, "MonitorProjectile", ref _monitorProjectile);
+        AssignMethod(delegates, "UnMonitorProjectile", ref _unMonitorProjectile);
+        AssignMethod(delegates, "GetProjectileState", ref _getProjectileState);
+        AssignMethod(delegates, "GetProjectilesLockedOn", ref _getProjectilesLockedOn);
+
+        AssignMethod(delegates, "FireWeaponOnce", ref _fireWeaponOnce);
+        AssignMethod(delegates, "ToggleWeaponFire", ref _toggleWeaponFire);
+        AssignMethod(delegates, "IsWeaponReadyToFire", ref _isWeaponReadyToFire);
+        AssignMethod(delegates, "GetMaxWeaponRange", ref _getMaxWeaponRange);
+        AssignMethod(delegates, "GetWeaponScope", ref _getWeaponScope);
+
+        AssignMethod(delegates, "GetCurrentPower", ref _currentPowerConsumption);
+        IsReady = true;
+        return true;
+    }
+
+    private void AssignMethod<T>(IReadOnlyDictionary<string, Delegate> delegates, string name, ref T field) where T : class
+    {
+        if (delegates == null)
+        {
+            field = null;
+            return;
+        }
+        Delegate del;
+        if (!delegates.TryGetValue(name, out del))
+            throw new Exception($"{GetType().Name} :: Couldn't find {name} delegate of type {typeof(T)}");
+        field = del as T;
+        if (field == null)
+            throw new Exception(
+                $"{GetType().Name} :: Delegate {name} is not type {typeof(T)}, instead it's: {del.GetType()}");
+    }
+
+    public void GetSortedThreats(IMyTerminalBlock pbBlock, IDictionary<MyDetectedEntityInfo, float> collection) =>
+        _getSortedThreats?.Invoke(pbBlock, collection);
+
+    public MyDetectedEntityInfo? GetAiFocus(long shooter, int priority = 0) => _getAiFocus?.Invoke(shooter, priority);
+
+    public bool HasCoreWeapon(IMyTerminalBlock weapon) => _hasCoreWeapon?.Invoke(weapon) ?? false;
+
+    public bool GetTurretTargetTypes(IMyTerminalBlock weapon, ICollection<string> collection) =>
+        _getTurretTargetTypes?.Invoke(weapon, collection, 0) ?? false;
+
+    public void SetTurretTargetTypes(IMyTerminalBlock weapon, ICollection<string> collection) =>
+        _setTurretTargetTypes?.Invoke(weapon, collection, 0);
+}
